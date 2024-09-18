4f9b780da3c1a45bf5e4502bad9401392be7c95e
Valentin Shevchenko <valwel@bk.ru>
2024-09-18 14:14:52 +0300

    header

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,17 +1,62 @@
 <template>
-  <div class="header container">
-    <!-- <div class="header__logo"></div>
-    <nav class="header__nav">
-      <router-link to="/">Почему мы?</router-link>
-      <router-link to="/rooms">Номера</router-link>
-    </nav>
-    <router-view/> -->
-  </div>
+  <header class="container" :class="{ bg: !burger }">
+    <div class="header">
+      <router-link to="/" class="header__logo">
+        <img src="@/assets/img/logo.svg" alt="" />
+      </router-link>
+      <div class="header__menu">
+        <HeaderNavMenu class="mobile"></HeaderNavMenu>
+        <div @click="burger = !burger" class="desktop">
+          <img v-show="burger" src="@/assets/img/open-burger.svg" alt="" />
+          <img v-show="!burger" src="@/assets/img/close-burger.svg" alt="" />
+        </div>
+      </div>
+    </div>
+    <HeaderNavMenu v-show="!burger" class="desktop"></HeaderNavMenu>
+  </header>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import HeaderNavMenu from "@/components/HeaderNavMenu.vue";
+import { ref } from "vue";
 
+const burger = ref(true);
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "@/assets/styles/main.scss";
+
+.bg {
+  @media (max-width: 768px) {
+    background-color: $yellow;
+    height: 100vh;
+  }
+}
+
+.mobile {
+  @media (max-width: 768px) {
+    display: none;
+  }
+}
+
+.desktop {
+  @media (min-width: 769px) {
+    display: none;
+  }
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding-top: 12px;
+  padding-bottom: 12px;
+
+  @media (max-width: 768px) {
+  }
+
+  &__menu {
+    display: flex;
+    align-items: center;
+  }
+}
 </style>
---
+++ src/components/HeaderNavMenu.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="nav h5">
+    <router-link to="/">Почему мы?</router-link>
+    <router-link to="/rooms">Номера</router-link>
+    <a href="#">Отзывы</a>
+    <a href="#">Как нас найти</a>
+    <div class="nav__contacts">
+      <a href="tel:+78003335599" class="nav__contacts-phone">8 (800) 333-55-99</a>
+      <ul class="nav__contacts-links">
+        <li><img src="@/assets/img/fb.svg" alt=""></li>
+        <li><img src="@/assets/img/inst.svg" alt=""></li>
+        <li><img src="@/assets/img/vk.svg" alt=""></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/main.scss';
+.nav {
+  display: flex;
+  gap: 20px;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    margin-top: 50px;
+    flex-direction: column;
+    align-items: start;
+    gap: 25px;
+  }
+
+  &__contacts {
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+
+  &__contacts-phone {
+    font-weight: 400;
+  }
+
+  &__contacts-links {
+    display: flex;
+    gap: 20px;
+  }
+}
+</style>
